dc343d6f0d9838bc246a764944cc526682ed554e
P-ROGER06 <proger06@gmail.com>
2021-03-14 10:05:25 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+$movieTitle = 'Indiana Jones and the Last Crusade';
+    
+    echo $movieTitle;
+
+    echo gettype($movieTitle);
+
+$alreadySaw = true;
+
+    echo $alreadySaw;
+
+    echo gettype($alreadySaw);
+
+$releaseDate = '1989';
+
+    echo $releaseDate; 
+
+    echo gettype($releaseDate);
+
+$imdbNote = '8,2/10 ';
+
+    echo $imdbNote; 
+
+    echo gettype($imdbNote);
+
+?>
